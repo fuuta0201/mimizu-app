389aa2db171f42edcef64105626c0ff012ded1fe
fuuta0201 <satofuta0201@gmail.com>
2024-07-04 18:47:09 +0900

    add: gacha

--- screens/Gacha.js
+++ screens/Gacha.js
@@ -1,12 +1,39 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import Normal from '../assets/gacha/gacha-image-01.png';
+import Premium from '../assets/gacha/gacha-image-02.png';
 
 const Gacha = () => {
   return (
-    <View>
-      <Text>Gacha Page</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.box}>
+        <Image source={Normal} style={styles.image}/>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.box}>
+        <Image source={Premium} style={styles.image}/>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 40,
+  },
+  box: {
+    width: 345,
+    height: 275,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
+});
 
 export default Gacha
