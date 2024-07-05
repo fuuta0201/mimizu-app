9ae8f4e354d5577c7318f7b9a466c82f7c5fb2e4
fuuta0201 <satofuta0201@gmail.com>
2024-07-05 14:02:07 +0900

    feat: home commentPanel

--- screens/Home.js
+++ screens/Home.js
@@ -6,6 +6,9 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Mimizu ver={2}></Mimizu>
+      <View style={styles.commentPanel}>
+        <Text style={styles.text}>ミネラルをたくさん含んでいるよ</Text>
+      </View>
     </View>
   )
 }
@@ -18,6 +21,27 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'flex-end',
+    position: 'relative'
+  },
+  commentPanel: {
+    width: 210,
+    height: 70,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 280,
+    right: 20,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '700',
+    textAlign: 'center',
   }
 });
 
